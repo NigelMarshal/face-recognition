775f6e9b5ff5d2c9078dd3a2ac917170c000b9f8
Nigel Marshal <nigelmarshal@gmail.com>
2018-06-06 16:12:18 +0400

    Created initial front-end layer with responsive styling and first layer of libs

---
+++ README.md
@@ -0,0 +1,3 @@
+Gaining some traction on my React learning journey.
+
+Exploring more into the world of Backend, APIs and React.
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+          <div className='white f3'>
+            {'Nigel, you rank ....'}
+          </div>
+          <div className='white f1'>
+            {'#1'}
+          </div>
+    </div>
+  );
+}
+
+export default Rank;
