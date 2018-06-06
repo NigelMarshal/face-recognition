1c4013270025eb6b3ac15455ebce82875b647748
Nigel Marshal <nigelmarshal@gmail.com>
2018-06-06 17:38:32 +0400

    Connected picture to input events. Connected to Clarifai API. Established clarifai model for facedetection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'de8fe79f726445e4b1b733a7646bbe87'
+});
 
 const particlesOptions = {
   particles: {
@@ -22,16 +28,28 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      inpu: '',
+      input: '',
+      imageUrl: '',
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({input: event.target.value});
   }
 
-onSubmit = () =>{
-  console.log('click');
+onSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models
+  .predict(
+    Clarifai.FACE_DETECT_MODEL,
+     this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 }
 render() {
   return (<div className="App">
@@ -44,10 +62,8 @@ render() {
       onInputChange={this.onInputChange}
        onSubmit={this.onSubmit}
      />
+     <FaceRecognition imageUrl={this.state.imageUrl} />
 
-    {/*
-        <FaceRecognition /> */
-    }
   </div>);
 }
 }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onSubmit }) => {
   return (
     <div>
       <p className='f3'>
-          {'This will detect your faces in your picture in a not creepy way. Try it out!'}
+          {'This will detect faces in your picture in a totally not creepy way. Try it out!'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
