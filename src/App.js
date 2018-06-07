d53954059385d1496b88bcb1f6fccd6a731ab851
Nigel Marshal <nigelmarshal@gmail.com>
2018-06-07 22:39:06 +0400

    Added registration and improved routing for forms and navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -32,11 +33,12 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
-calculateFaceLocation = (data) => {
+  calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
@@ -47,47 +49,58 @@ calculateFaceLocation = (data) => {
       rightCol: width - (clarifaiFace.right_col) * width,
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
-}
+  }
 
-displayFaceBox = (box) => {
-  this.setState({box: box});
-}
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
-onSubmit = () => {
-  this.setState({imageUrl: this.state.input});
-  app.models
-  .predict(
-    Clarifai.FACE_DETECT_MODEL,
-     this.state.input)
-     .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-     .catch (err => console.log(err));
-}
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch (err => console.log(err));
+    }
 
-onrouteChange = (route) => {
-  this.setState({route: route});
-}
-
-render() {
-  return (<div className="App">
-    <Particles className='particles' params={particlesOptions}/>
-    <Navigation onrouteChange={this.onrouteChange}/>
-    { this.state.route === 'signin' ?
-      <SignIn onrouteChange={this.onrouteChange}/>
-      : <div>
-        <Logo/>
-        <Rank/>
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-           onSubmit={this.onSubmit}
-         />
-         <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
-       </div>
+    onrouteChange = (route) => {
+      if (route === 'signout'){
+        this.setState({isSignedIn: false})
+      } else if (route === 'home'){
+        this.setState({isSignedIn: true})
       }
-    </div>);
+      this.setState({route: route});
+    }
+
+    render() {
+      const {isSignedIn, imageUrl, route, box} = this.state;
+      return (<div className="App">
+        <Particles className='particles' params={particlesOptions}/>
+        <Navigation isSignedIn={isSignedIn}onrouteChange={this.onrouteChange}/>
+        { route === 'home'
+        ? <div>
+          <Logo/>
+          <Rank/>
+          <ImageLinkForm
+            onInputChange={this.onInputChange}
+            onSubmit={this.onSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+        </div>
+        : (
+          route === 'signin'
+          ? <SignIn onrouteChange={this.onrouteChange}/>
+          : <Register onrouteChange={this.onrouteChange}/>
+        )
+      }
+    </div>
+  );
 }
 }
 
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-
-const SignIn = ({onrouteChange}) => {
+const Register = ({onrouteChange}) => {
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shaddow-5 center">
       <main className="pa4 black-80">
         <div className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
+              </div>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
               <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -21,11 +24,8 @@ const SignIn = ({onrouteChange}) => {
                 <input
                   onClick={() => onrouteChange('home')}
                   className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                  type="submit" value="Sign in"
+                  type="submit" value="Register"
                 />
-                </div>
-                <div className="lh-copy mt3">
-                  <p className="f6 link dim black db pointer" onClick={() => onrouteChange('register')}>Register</p>
                 </div>
               </div>
             </main>
@@ -33,4 +33,4 @@ const SignIn = ({onrouteChange}) => {
         );
       }
 
-      export default SignIn;
+      export default Register;
