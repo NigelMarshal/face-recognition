44e2ffb0ab7fabe173edf149dc383f1e5bbb6805
Nigel Marshal <nigelmarshal@gmail.com>
2018-06-14 03:58:03 +0400

    deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
@@ -9,10 +8,6 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: 'de8fe79f726445e4b1b733a7646bbe87'
-});
 
 const particlesOptions = {
   particles: {
@@ -80,13 +75,17 @@ class App extends Component {
 
   onButtonSubmit = () => {
    this.setState({imageUrl: this.state.input});
-   app.models
-     .predict(
-       Clarifai.FACE_DETECT_MODEL,
-       this.state.input)
+   fetch('https://rocky-cliffs-12192.herokuapp.com/imageurl', {
+     method: 'post',
+     headers: {'Content-Type': 'application/json'},
+     body: JSON.stringify({
+       input: this.state.input
+     })
+   })
+   .then(response => response.json())
      .then(response => {
        if (response) {
-         fetch('http://localhost:3000/image', {
+         fetch('https://rocky-cliffs-12192.herokuapp.com/image', {
            method: 'put',
            headers: {'Content-Type': 'application/json'},
            body: JSON.stringify({
